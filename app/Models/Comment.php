e633b8cbbe48641804b16751e9ebdc23b4fae55a
Andrew Gas <prozius11@gmail.com>
2022-10-31 15:24:19 +0300

    add mutators

--- app/Models/Comment.php
+++ app/Models/Comment.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Casts\Attribute;
 
 class Comment extends Model
 {
@@ -15,4 +16,17 @@ class Comment extends Model
      * @var array<int, string>
      */
     protected $fillable = ['name', 'text', 'post_id'];
+
+
+    /**
+     * Get the user's first name.
+     *
+     * @return \Illuminate\Database\Eloquent\Casts\Attribute
+     */
+    protected function name(): Attribute
+    {
+        return Attribute::make(
+            get: fn($value) => ucfirst($value),
+        );
+    }
 }
--- app/Models/Post.php
+++ app/Models/Post.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
@@ -15,4 +16,16 @@ class Post extends Model
      * @var array<int, string>
      */
     protected $fillable = ['author_id', 'title', 'short_title', 'img', 'descr'];
+
+    /**
+     * Get the post's title.
+     *
+     * @return \Illuminate\Database\Eloquent\Casts\Attribute
+     */
+    protected function title(): Attribute
+    {
+        return Attribute::make(
+            get: fn($value) => ucfirst($value),
+        );
+    }
 }
